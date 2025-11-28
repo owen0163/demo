0d09fadcbcbc9535481835d8faed9b8c3cc28c43
owen <saleumxayowen@gmail.com>
2025-11-28 10:16:47 +0700

    Update main.go

--- array/main.go
+++ array/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	var a = [...]int{-1, 0, 1, 2}
 	var s []int
 
-	s = make([]int, 0)
+	// s = make([]int, 0)
 
-	if s == nil {
-		fmt.Println("it's nill")
-	} else {
-		fmt.Println("it's not nil")
-	}
+	// if s == nil {
+	// 	fmt.Println("it's nill")
+	// } else {
+	// 	fmt.Println("it's not nil")
+	// }
+	//*********************************************************
+	// s = append(s, -1, 0, 1, 2)
+	// fmt.Println(len(s))
+	//--------------------------------------------------------
 
+	s = a[1:3]
+	fmt.Println(len(s))
+	fmt.Println(s)
 }
